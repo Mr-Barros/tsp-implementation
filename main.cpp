2efaccb33bbcc724fee567dd3cf08b9a4fc848c8
Davi Martignoni Barros <davimartignonibarros@gmail.com>
2026-03-08 18:45:26 -0300

    feat: chamar solução na função main
    Utiliza a função Solution::find_tsp_tour para encontrar um caminho TSP na instância especificada pelo usuário. Em seguida, exibe o caminho no terminal.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include "FileDecoder.hpp"
 #include "TSPInstance.hpp"
+#include "Solution.hpp"
 
 int main(void) {
     using namespace std;
@@ -17,6 +18,15 @@ int main(void) {
             TSPInstance tsp = decoder.decode_input_file();
             Graph g = tsp.build_graph();
             cout << "Tamanho do caminho canônico: " << g.canonical_tour_length() << endl;
+            
+            Solution sol;
+            vector<int> tsp_tour = sol.find_tsp_tour(g);
+
+            cout << "Caminho TSP encontrado:" << endl;
+            for (int i = 0; i < tsp_tour.size(); i++) {
+                cout << tsp_tour[i] + 1 << " \n"[i == tsp_tour.size() - 1];
+            }
+
         } catch (const std::string& ex) {
             cout << ex << endl;
         }
